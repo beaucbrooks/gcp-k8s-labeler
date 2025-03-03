6c743bee4c991103017780edd340de3420f58cd3
Beau B <beaub@example.com>
2025-03-03 14:54:59 -0800

    fix call out globals, reduce spacing

--- powershell/label_analysis.ps1
+++ powershell/label_analysis.ps1
@@ -1,3 +1,13 @@
+. "$PSScriptRoot/logging.ps1"
+$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+$global:LOG_FILE = "$PSScriptRoot\gke_cluster_export_$timestamp.log"
+$global:csvOutputPath = "$PSScriptRoot\gke_cluster_labels_$timestamp.csv"
+$global:MISING_CONFIGS = @()
+$global:PROJECT_ID = Get-Content $env:GOOGLE_APPLICATION_CREDENTIALS | ConvertFrom-Json | Select-Object -ExpandProperty project_id
+$global:TOTAL_CLUSTERS = 0
+$global:SUCCESSFUL_EXPORTS = 0
+$global:FAILED_EXPORTS = 0
+
 # Put your 'secret' values in a file named .env
 # with the format of KEY=VALUE .NO QUOTES NO SPACES.
 if (Test-Path "$PSScriptRoot/.env") {
@@ -6,24 +16,24 @@ if (Test-Path "$PSScriptRoot/.env") {
         Set-Item -Path "env:$key" -Value $value
     }
 }
-. "$PSScriptRoot/logging.ps1"
-$env:Path += ";$env:USERPROFILE\.kube" # expecting kubectl to either be here, or in your path
-$credentialsPath = $env:GOOGLE_APPLICATION_CREDENTIALS
-$projectId = Get-Content $credentialsPath | ConvertFrom-Json | Select-Object -ExpandProperty project_id
-$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$logFile = "$PSScriptRoot\gke_cluster_export_$timestamp.log"
-$csvOutputPath = "$PSScriptRoot\gke_cluster_labels_$timestamp.csv"
-$missingConfigs = @()
-$global:TOTAL_CLUSTERS = 0
-$global:SUCCESSFUL_EXPORTS = 0
-$global:FAILED_EXPORTS = 0
+else {
+    Write-Log "No .env file found, not sure where you're getting your secrets/credentials from. Exiting..." -Level "ERROR"
+    exit 1
+}
+
+if (Test-Path "$env:USERPROFILE\.kube") {
+    $env:Path += ";$env:USERPROFILE\.kube"
+}
+else {
+    Write-Log "Kube config not found, without it you will not be able to pull label info. Exiting..." -Level "ERROR"
+    exit 1
+}
 
 function Connect-GCP {
     try {
         Write-Log "Attempting GCP login with Service Account" 
-        gcloud auth activate-service-account --key-file=$credentialsPath
-        gcloud config set project $projectId
-        Write-Log "GCP login successful for project: $projectId" 
+        gcloud auth activate-service-account --key-file=$env:GOOGLE_APPLICATION_CREDENTIALS
+        gcloud config set project $global:PROJECT_ID
     }
     catch {
         Write-Log "Failed to login to GCP: $_" -Level "ERROR"
@@ -98,12 +108,9 @@ function Set-NamespaceLabel {
     )
 
     Write-Host "Adding label '$labelKey=$labelValue' to namespace '$namespace' in cluster '$context'..."
-
     kubectl config use-context $context | Out-Null
-
     $command = "kubectl label namespace $namespace $labelKey=$labelValue --overwrite"
     Invoke-Expression $command
-
     Write-Host "Label applied successfully!"
 }
 
@@ -114,30 +121,38 @@ function Set-LabelForAllNamespaces {
     )
 
     $contexts = Get-ClusterContexts
-
     foreach ($context in $contexts) {
         Write-Host "Processing cluster: $context"
-        
         kubectl config use-context $context | Out-Null
-
         $namespaces = kubectl get namespaces -o json | ConvertFrom-Json
         foreach ($ns in $namespaces.items) {
             $namespaceName = $ns.metadata.name
             Write-Host "Labeling namespace: $namespaceName in cluster: $context"
-
             Set-NamespaceLabel -context $context -namespace $namespaceName -labelKey $labelKey -labelValue $labelValue
         }
     }
 }
 
+function Get-GKEClusters {
+    param(
+        [string]$projectId
+    )
+    $clusters = $null
+    Write-Log "Getting GKE clusters in project $projectId"
+    try {
+        $clusters = gcloud container clusters list --format="json" | ConvertFrom-Json
+    }
+    catch {
+        Write-Log "Failed to get GKE clusters: $_" -Level "ERROR"
+    }
+    return $clusters
+}
+
 function Get-ClusterInfo {
     $clusterInfoResults = [System.Collections.Generic.List[PSCustomObject]]::new()
+    $clusters = Get-GKEClusters -projectId $global:PROJECT_ID 
     try {
-        # Get GKE clusters
-        Write-Log "Getting GKE clusters in project $projectId"
-        $clusters = gcloud container clusters list --format="json" | ConvertFrom-Json
         if ($clusters) {
-            Write-Log "Found $($clusters.Count) GKE clusters in project $projectId"
             foreach ($cluster in $clusters) {
                 $global:TOTAL_CLUSTERS++
                 $clusterName = $cluster.name
@@ -146,7 +161,7 @@ function Get-ClusterInfo {
                     Write-Log "Processing cluster: $clusterName in $location"
                     $clusterDetail = gcloud container clusters describe $clusterName --region=$location --format="json" | ConvertFrom-Json
                     $clusterInfo = [PSCustomObject]@{
-                        project    = $projectId
+                        project    = $global:PROJECT_ID
                         name       = $clusterName
                         location   = $location
                         version    = $clusterDetail.currentMasterVersion
@@ -163,17 +178,17 @@ function Get-ClusterInfo {
                 }
                 catch {
                     Write-Log "Failed to analyze cluster $clusterName. Error: $_" -Level "ERROR"
-                    $missingConfigs += "$projectId : $clusterName"
+                    $global:MISSING_CONFIGS += "$global:PROJECT_ID : $clusterName"
                     $global:FAILED_EXPORTS++
                 }
             }
         }
         else {
-            Write-Log "No clusters found in project $projectId" -Level "INFO"
+            Write-Log "No clusters found in project $global:PROJECT_ID" -Level "INFO"
         }
     }
     catch {
-        Write-Log "Failed to process project $projectId. Error: $_" -Level "ERROR"
+        Write-Log "Failed to process project $global:PROJECT_ID. Error: $_" -Level "ERROR"
         $global:FAILED_EXPORTS++
     }
     return $clusterInfoResults
@@ -213,18 +228,18 @@ function Write-Summary {
     Write-Log "Successful exports: $global:SUCCESSFUL_EXPORTS"
     Write-Log "Failed exports: $global:FAILED_EXPORTS"
 
-    if ($missingConfigs.Count -gt 0) {
+    if ($global:MISING_CONFIGS.Count -gt 0) {
         Write-Log "`n=== FAILED EXPORTS ===" -Level "ERROR"
-        foreach ($missing in $missingConfigs) {
+        foreach ($missing in $global:MISING_CONFIGS) {
             Write-Log "  - $missing" -Level "ERROR"
         }
     }
 
     Write-Log "`nCluster analysis complete"
-    Write-Log "Results exported to: $csvOutputPath"
-    Write-Log "Full execution log available at: $logFile"
+    Write-Log "Results exported to: $global:csvOutputPath"
+    Write-Log "Full execution log available at: $global:LOG_FILE"
 }
 
 Connect-GCP
-Get-CombinedInformation | Export-Csv -Path $csvOutputPath -NoTypeInformation 
+Get-CombinedInformation | Export-Csv -Path $global:csvOutputPath -NoTypeInformation 
 Write-Summary
